2317dc85cde1a3074064b0b597e554d4dfe6a22d
Alfian Ramadani <Alfianr792@gmail.com>
2023-10-19 07:44:56 +0700

    commit 1

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ALfian</title>
+    <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/dist/js/bootstrap.min.js">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <b>This is Alfian Website</b>
+    
 </body>
 </html>
